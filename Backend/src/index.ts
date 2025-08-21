bfc76b937e496e94bbd091abed545ce93062dabe
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-08-21 12:28:25 +0530

    feat: add all the configuration

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,13 +1,31 @@
+import db from "./config/db.mogo.js"
 import express from "express"
-import { connectToDatabase } from "./astraDB/connection.js"
-const app = express()
+import startup from "./startup/route.js"
+import * as dotenv from "dotenv"
+import cors from "cors"
+import { connectToDatabase } from "./config/db.astra.js"
+
 const database = connectToDatabase()
+dotenv.config()
 
+const app = express()
+
+// CORS middleware
+app.use(cors({
+    origin: '*', 
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
+    exposedHeaders: ["Authorization"],
+    credentials: true 
+}))
+startup(app)
+
+// mongoDB connection
+db
+// astraDB connection
 database
-
-
-
-
-app.listen(3000, () => {
-    console.log("listening on Port 3000");
+const port = 3000
+app.listen(port, () => {
+    console.log(`listening on pot: ${port}`);
+    
 })

dca88b0229b77509d3c39943128a1d9eb945d94f
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-08-26 16:15:45 +0530

    feat: comment the startp page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,31 +1,45 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Startup } from './pages/startup'
+import { Home } from './pages/Home'
+import { ThemeProvider } from './components/ui/theme-provider'
+import { Header } from './components/Header'
+import { Chat } from './pages/Chat'
+import LoginPage from './pages/Login'
+import SignUpPage from './pages/Signup'
 
 function App() {
   const [showStartup, setShowStartup] = useState(true)
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-        setShowStartup(false)
-    }, 5000)
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //       setShowStartup(false)
+  //   }, 5000)
 
-    return () => clearTimeout(timer)
-  },[])
+  //   return () => clearTimeout(timer)
+  // },[])
 
-  if(showStartup){
-    return <Startup/>
-  }
+  // if(showStartup){
+  //   return <Startup/>
+  // }
 
   return (
-    <div>
-      <Router>
-        <Routes>
-          {/* <Route path='/' element={<Home/>}/> */}
-        </Routes>
-      </Router>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className='bg-black'>
+        <Router>
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/chat' element={<Chat/>}/>
+            <Route path='/signIn' element={<LoginPage/>}/>
+            <Route path='/register' element={<SignUpPage/>}/>
+
+
+          </Routes>
+        </Router>
+      </div>
+    </ThemeProvider>
   )
 }
 

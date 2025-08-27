e9522d7a6d442086044d7f26f784a89c95c91e16
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-08-27 15:31:53 +0530

    feat: make it responsive for all devices

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -2,22 +2,10 @@ import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="bg-muted relative hidden lg:block">
-        <img
-          src="https://cf-img-a-in.tosshub.com/sites/visualstory/wp/2024/08/arjun-krishna-during-mahabharata-4.jpg?size=*:900"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.8] dark:"
-        />
+    <div className="bg-black flex flex-col items-center justify-center px-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
       </div>
-      <div className="flex flex-col gap-4 p-6 md:p-10 bg-[#151b23]">
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <LoginForm />
-          </div>
-        </div>
-      </div>
-      
     </div>
   )
 }

d5e078ed2ff9b9cf7502246e2405020310115571
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-08-27 15:32:07 +0530

    feat: make it responsive for all devices

--- Frontend/src/pages/Signup.tsx
+++ Frontend/src/pages/Signup.tsx
@@ -1,23 +1,11 @@
 import { SignUpForm } from "@/components/Signup-form"
 
-export default function SignUpPage() {
+export default function LoginPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2 bg-[#151b23]">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <SignUpForm />
-          </div>
-        </div>
+    <div className="bg-black flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <SignUpForm />
       </div>
-      <div className="bg-muted relative hidden lg:block">
-        <img
-          src="https://cf-img-a-in.tosshub.com/sites/visualstory/wp/2024/08/arjun-krishna-during-mahabharata-4.jpg?size=*:900"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.8] dark:"
-        />
-      </div>
-      
     </div>
   )
 }

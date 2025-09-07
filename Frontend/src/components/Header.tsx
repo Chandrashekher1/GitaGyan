453796a69f197117f6a5fbee1f55205253517a47
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-09-07 14:28:08 +0530

    feat: add dynamic url path of images

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import { Navbar } from "./Navbar";
+import { Lotus_Image } from "@/utils/constant";
 
 export function Header() {
     return (
         <div className="border-b border-border/50 backdrop-blur-md sticky top-0 z-50 shadow-elevated flex justify-between px-16 py-4 shadow-lg bg-white/70">
             <div>
                 <Link to="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity group">
-                    <img src="https://www.freevector.com/uploads/vector/preview/2916/FreeVector-Lotus-Icon.jpg" alt="" className="w-10 h-10 rounded-full" />
+                    <img src={`${Lotus_Image}`} alt="" className="w-10 h-10 rounded-full" />
                     <span className="font-display font-bold text-2xl sacred-text">GitaGyan</span>
                 </Link>
             </div>

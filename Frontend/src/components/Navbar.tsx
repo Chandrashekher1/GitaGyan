b848ba0a4bbafb21ef0d18595743698865aa19bd
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-09-07 22:21:20 +0530

    style: adjust margin and padding

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
+import { LogOutIcon, UserIcon } from "lucide-react";
 
 export function Navbar() {
+    const navigate = useNavigate()
+
+    const handleLogOut = () => {
+        localStorage.removeItem("token");
+        navigate("/")
+    }
+
     return(
         <div className="hidden md:flex items-center gap-6">
-            <Link to="/chat">Chat</Link>
-
+            {/* <Link to="/chat">Chat</Link> */}
             <div>
-                <Button>Profile Icon</Button>
-                <Button>SignIn Icon</Button>
+                <Button variant="outline" className="mx-4 rounded-full"><UserIcon/></Button>
+                <Button variant="outline" className="rounded-full" onClick={handleLogOut}><LogOutIcon/></Button>
 
             </div>
         </div>

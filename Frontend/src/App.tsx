08da529ab770e0be285ac09dc72a4db288f2bb64
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-09-07 22:34:59 +0530

    feat: add path of Login and Signup page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { ThemeProvider } from './components/ui/theme-provider'
 import { Chat } from './pages/Chat'
+import { Login } from './pages/Login'
+import { SignUp } from './pages/SignUp'
 
 function App() {
 
@@ -13,6 +15,9 @@ function App() {
           <Routes>
             <Route path='/' element = {<Home/>}/>
             <Route path='/chat' element = {<Chat/>}/>
+            <Route path='/login' element = {<Login/>}/>
+            <Route path='/signup' element = {<SignUp/>}/>
+
           </Routes>
         </Router>
       </div>

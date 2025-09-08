f00a4f295ff1922332f22874827f4df468d548c4
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-09-08 23:10:14 +0530

    style: adjust margin and padding of header

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -4,11 +4,11 @@ import { Lotus_Image } from "@/utils/constant";
 
 export function Header() {
     return (
-        <div className="border-b border-border/50 backdrop-blur-md sticky top-0 z-50 shadow-elevated flex justify-between px-2 md:px-16 py-4 shadow-lg bg-white/70">
+        <div className="border-b border-border/50 backdrop-blur-md sticky top-0 z-50 shadow-elevated flex justify-between px-2 md:px-16 md:py-4 py-2 shadow-lg bg-white/70">
             <div>
-                <Link to="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity group">
-                    <img src={`${Lotus_Image}`} alt="" className="w-10 h-10 rounded-full" />
-                    <span className="font-display font-bold text-2xl sacred-text">GitaGyan</span>
+                <Link to="/" className="flex items-center gap-1 md:gap-2 hover:opacity-80 transition-opacity group">
+                    <img src={`${Lotus_Image}`} alt="" className="md:w-10 md:h-10 h-8 w-8 rounded-full" />
+                    <span className="font-display font-bold text-2xl font-serif text-primary">GitaGyan</span>
                 </Link>
             </div>
             <Navbar/>

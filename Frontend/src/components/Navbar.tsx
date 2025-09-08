a2deb58f777cd50ebbafd63c0b51b635e7f7f6c8
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-09-08 10:52:14 +0530

    feat: add hover effect on nav

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -3,21 +3,36 @@ import { Button } from "./ui/button";
 import { LogOutIcon, UserIcon } from "lucide-react";
 
 export function Navbar() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const handleLogOut = () => {
-        localStorage.removeItem("token");
-        navigate("/")
-    }
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
 
-    return(
-        <div className="hidden md:flex items-center gap-6">
-            {/* <Link to="/chat">Chat</Link> */}
-            <div>
-                <Button variant="outline" className="mx-4 rounded-full"><UserIcon/></Button>
-                <Button variant="outline" className="rounded-full" onClick={handleLogOut}><LogOutIcon/></Button>
+  return (
+    <div className="hidden md:flex items-center gap-6 relative">
+      <div className="relative group">
+        <Button variant="outline" className=" rounded-full">
+          <UserIcon />
+        </Button>
+        <p className="absolute left-1/2 -translate-x-1/2 top-12 text-sm sacred-text px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
+          Profile
+        </p>
+      </div>
 
-            </div>
-        </div>
-    )
+      <div className="relative group">
+        <Button
+          variant="outline"
+          className="rounded-full"
+          onClick={handleLogOut}
+        >
+          <LogOutIcon />
+        </Button>
+        <p className="absolute left-1/2 -translate-x-1/2 top-12 text-sm sacred-text px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
+          Logout
+        </p>
+      </div>
+    </div>
+  );
 }

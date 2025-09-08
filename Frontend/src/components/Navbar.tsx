69cc436b05b24b3fe4e9395b4627bd2dda55eaa7
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-09-08 23:39:08 +0530

    style: change icons color

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -13,23 +13,23 @@ export function Navbar() {
   return (
     <div className="hidden md:flex items-center gap-6 relative">
       <div className="relative group">
-        <Button variant="secondary" className=" rounded-full">
+        <Button variant="default" className=" rounded-full">
           <UserIcon />
         </Button>
-        <p className="absolute left-1/2 -translate-x-1/2 top-12 text-sm sacred-text px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
+        <p className="absolute left-1/2 -translate-x-1/2 top-12 text-sm text-primary-foreground bg-primary px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
           Profile
         </p>
       </div>
 
       <div className="relative group">
         <Button
-          variant="secondary"
+          variant="default"
           className="rounded-full"
           onClick={handleLogOut}
         >
           <LogOutIcon />
         </Button>
-        <p className="absolute left-1/2 -translate-x-1/2 top-12 text-sm sacred-text px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
+        <p className="absolute left-1/2 -translate-x-1/2 top-12 text-sm text-primary-foreground bg-primary px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
           Logout
         </p>
       </div>

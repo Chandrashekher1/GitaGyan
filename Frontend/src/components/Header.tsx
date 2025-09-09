8893205d29ae2b6898dbc79117060cdb26548351
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-09-09 23:28:12 +0530

    style: adjust margin and padding to header

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Lotus_Image } from "@/utils/constant";
 
 export function Header() {
     return (
-        <div className="border-b border-border/50 backdrop-blur-md sticky top-0 z-50 shadow-elevated flex justify-between px-2 md:px-16 md:py-4 py-2 shadow-lg bg-white/70">
+        <div className="border-b border-border/50 backdrop-blur-md sticky top-0 z-50 shadow-elevated flex justify-between px-2 md:px-16 md:py-4 py-3 shadow-lg bg-white/70">
             <div>
                 <Link to="/" className="flex items-center gap-1 md:gap-2 hover:opacity-80 transition-opacity group">
                     <img src={`${Lotus_Image}`} alt="" className="md:w-10 md:h-10 h-8 w-8 rounded-full" />

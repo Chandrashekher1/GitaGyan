be35204e8f7bec394d8f68d482d5062297908e49
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-09-13 21:44:43 +0530

    feat: add Language Provider

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,20 +4,34 @@ import { Home } from './pages/Home'
 import { Chat } from './pages/Chat'
 import { Login } from './pages/Login'
 import { SignUp } from './pages/SignUp'
+import About from './pages/About'
+import Chapters from './pages/Chapters'
+import Verses from './pages/Verses'
+import { Header } from './components/Header'
+import Meditation from './pages/Meditations'
+import { LanguageProvider } from './context/Language'
 
 function App() {
 
   return (
       <div>
-        <Router>
+        <LanguageProvider>
+            <Router>
+          <Header/>
           <Routes>
             <Route path='/' element = {<Home/>}/>
             <Route path='/chat' element = {<Chat/>}/>
             <Route path='/login' element = {<Login/>}/>
             <Route path='/signup' element = {<SignUp/>}/>
+            <Route path='/about' element = {<About/>}/>
+            <Route path='/chapters' element = {<Chapters/>}/>
+            <Route path='/verses' element = {<Verses/>}/>
+            <Route path='/meditation' element = {<Meditation/>}/>
+
 
           </Routes>
         </Router>
+        </LanguageProvider>
       </div>
   )
 }

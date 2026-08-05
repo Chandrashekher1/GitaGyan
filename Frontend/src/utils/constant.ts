f63bee4ffc52c35117357d5f8409fe49a4c27f14
Chandrashekher Prasad <146510072+Chandrashekher1@users.noreply.github.com>
2026-08-05 10:24:53 +0530

    Update API endpoint to gitagyan-hackathon-1.onrender.com

--- Frontend/src/utils/constant.ts
+++ Frontend/src/utils/constant.ts
@@ -1,2 +1,2 @@
-export const Backend_Url = "https://gitagyan-1.onrender.com/api"
+export const Backend_Url = "https://gitagyan-hackathon-1.onrender.com/api"
 export const Krishna_Flute = "/sounds/krishna_flute.mp3"
